81d4167a4424c40ef1091b5c7e9099cac17c75be
rbchipichipichapachapa-oss <rbchipichipichapachapa@gmail.com>
2025-09-17 17:28:51 +0200

    publish

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Real-madrid-fan',
+  assetPrefix: '/Real-madrid-fan/',
 };
 
 export default nextConfig;
